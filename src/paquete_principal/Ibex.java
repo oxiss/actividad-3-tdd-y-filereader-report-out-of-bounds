e9de7ed133cda265882c5dd0da1641436edaf2cc
Felipe Andrés Vásquez Tort <felipevasquez.tort@gmail.com>
2015-10-21 18:58:29 +0200

    Agregado un metodo util para pasar el dato de la fecha de un string a un Date.

--- src/paquete_principal/Ibex.java
+++ src/paquete_principal/Ibex.java
@@ -1,5 +1,31 @@
 package paquete_principal;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Ibex implements Interfaz_Ibex {
+	/**
+	 * Metodo que a partir de una cadena de caracteres espedifica se combierte a
+	 * la fecha correspondiente
+	 * 
+	 * @author Bufigol
+	 * @param entada
+	 *            Un string correspondiente a la fecha con el formato YYYYMMDD
+	 * @return La fecha correspondiente al String del parametro de entrada
+	 */
+	private Date paso_a_fecha(String entada) {
+		SimpleDateFormat paso_a_fecha = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			return paso_a_fecha.parse(entada.substring(6, 8) + "/"
+					+ entada.substring(4, 6) + "/" + entada.substring(0, 4));
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Se dio un error al entrar la fecha");
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
--- src/paquete_principal/Interfaz_Ibex.java
+++ src/paquete_principal/Interfaz_Ibex.java
@@ -1,5 +1,7 @@
 package paquete_principal;
 
-public interface Interfaz_Ibex {
 
+
+public interface Interfaz_Ibex {
+	
 }
