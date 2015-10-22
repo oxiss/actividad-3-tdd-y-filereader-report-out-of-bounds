e47b27cc63d7ff84906b612ee515884772512d6c
Felipe Andrés Vásquez Tort <felipevasquez.tort@gmail.com>
2015-10-22 12:52:17 +0200

    Commit con el trabajo de Santos a una rama especifica del projecto

--- src/paquete_principal/Ibex.java
+++ src/paquete_principal/Ibex.java
@@ -1,31 +1,78 @@
 package paquete_principal;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Ibex implements Interfaz_Ibex {
-	/**
-	 * Metodo que a partir de una cadena de caracteres espedifica se combierte a
-	 * la fecha correspondiente
-	 * 
-	 * @author Bufigol
-	 * @param entada
-	 *            Un string correspondiente a la fecha con el formato YYYYMMDD
-	 * @return La fecha correspondiente al String del parametro de entrada
-	 */
-	private Date paso_a_fecha(String entada) {
-		SimpleDateFormat paso_a_fecha = new SimpleDateFormat("dd/MM/yyyy");
+public class Ibex {
+
+	public float getCloseValue(int year, String path) {
+
+		
+		float aux = 0.0F;
+		float acumulado = 0.0F;
+		String cadena;
+		int contador=0;
+		FileReader f = null;
+		String yearSt = Integer.toString(year);
+		
 		try {
-			return paso_a_fecha.parse(entada.substring(6, 8) + "/"
-					+ entada.substring(4, 6) + "/" + entada.substring(0, 4));
-
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Se dio un error al entrar la fecha");
-			e.printStackTrace();
-			return null;
+			f = new FileReader(path);
+		} catch (FileNotFoundException e) {
+			System.err.println("el archivo no existe");
+			return -1;
 		}
-	}
+		//creamos un arraylist y comenzamos a leer el archivo
+		List<String> datos = new ArrayList<String>();
+		BufferedReader bReader = new BufferedReader(f);
 
+		try {
+			//añadimos los datos al Arraylist
+			while ((cadena = bReader.readLine()) != null) {
+				datos.add(cadena);
+			}
+			bReader.close();
+		} catch (IOException e) {
+			
+			System.err.println("Error al meter los datos en el Arraylist");
+			return -1;
+		}
+		// recorremos el Arraylist haciendo tokens en los lugares escogidos
+		for (String indice : datos) {
+			StringTokenizer st = new StringTokenizer(indice, ",");
+			//cogemos el tercer token (donde está la fecha)
+			for (int i = 0; i < 2; i++) {
+				st.nextToken();
+			}
+			// si el tercer token empieza con el año indicado...
+			if (st.nextToken().startsWith(yearSt)) {
+				// buscamos el 8º token (cierres)
+				for (int i = 0; i < 4; i++) {
+					st.nextToken();
+				}
+				//y lo vamos sumando en acumulado
+				aux = Float.parseFloat(st.nextToken());
+				acumulado = acumulado + aux;
+				//contador con 2 funciones: saber si hay filas para el año dado (!=0) y contar para el divisor
+				contador++;
+			}
+		}
+		acumulado = acumulado/contador;
+		//System.out.println(acumulado); (prueba para ver las medias)
+		// si han existido filas (>1)
+		if (contador>1){
+			return acumulado;
+		}
+		else{
+			// si no han existido filas (<1) 
+			System.err.println("la fecha introducida no es correcta");
+			return (float) 0.0;
+		}
+		
+	}
 }
+
