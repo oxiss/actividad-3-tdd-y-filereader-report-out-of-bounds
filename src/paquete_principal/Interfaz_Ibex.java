44ba680daa062d7f1329fd1095f57892bd641043
Miguel Angel Nuñez Aparicio <miguel.angel.nao@gmail.com>
2015-10-24 10:01:54 +0200

    Añadido el metodo final a falta de hacer las pruebas

--- src/paquete_principal/Interfaz_Ibex.java
+++ src/paquete_principal/Interfaz_Ibex.java
@@ -2,9 +2,21 @@ package paquete_principal;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
 
 public interface Interfaz_Ibex {
+
+	
+	public float getCloseValue(Date fecha, String path);// Punto 2
+	
+	public float getCloseValue(int year, String path);// Punto 3
+	
+	float getValue(Date fecha, int field, String path );//Punto 4
+	
+	//Punto 5 vvvvvvv
+
+
 	public float getCloseAvg(Date ini, Date fin, String path)
 			throws IOException, NumberFormatException, ParseException;
 
@@ -16,4 +28,11 @@ public interface Interfaz_Ibex {
 
 	public float getCloseAvg(String ini, String fin, String ruta)
 			throws NumberFormatException, IOException, ParseException;
+
+
+	//^^^^^^^^
+	
+	
+	public ArrayList<Date> getDatePoints(long points, String path);//Punto 6
+
 }
